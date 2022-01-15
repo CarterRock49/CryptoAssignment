a822c39ca3d85e6495dffb215b07823a11ac0027
CarterRock49 <CarterRock49@gmail.com>
2022-01-15 11:03:24 -0500

    member function to open the file added

--- CryptoCurrencies.cpp
+++ CryptoCurrencies.cpp
@@ -1,35 +1,52 @@
 // CryptoCurrencies.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <string>
+#include <iostream>
+#include <fstream>
 using namespace std;
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-/// An example function.
-string SomeFunc(string s)
-{
-    return "Hello " + s;
-}
 
 /// An example class.
-class SomeClass
-{
+class CryptoManager{
+private:
+    //change this to change what file is opened
+    const string fileDIR = "TestData/Coins.csv";
+    ifstream CryptoCSV;
+    struct CryptoCurrencies{
+        int rank;
+        string name;
+        string symbol;
+        long long int marketcap;
+    };
+
 public:
-    bool SomeMemberFunc(bool b)
-    {
-        // Return the NOT of b
-        return !b;
+    bool initialize(){
+        //opens the file that the fileDIR varible has stored in it
+        CryptoCSV.open(fileDIR);
+        //opens file and checks to make sure it was successful
+        if(CryptoCSV.is_open()){
+                return true;
+            } else {
+                return false;
+            }
     }
 };
 
 /// Testing starts here. This takes the place of "main()" ///
-TEST_CASE("<change this to something meaningful>")
-{
-    // This is an example test of function
-    CHECK(SomeFunc("world") == "Hello world");
-
+TEST_CASE("<change this to something meaningful>"){
     // This is an example test of a class and a public function
-    SomeClass sc;
-    CHECK(sc.SomeMemberFunc(true) == false);
+    CryptoManager CM;
+    CHECK(CM.initialize() == true);
+
 }
+
+/*
+    while(!CryptoCSV.eof()){
+        string entry;
+        getline(CryptoCSV, entry, ',');
+        cout << entry << endl;
+    }
+*/
